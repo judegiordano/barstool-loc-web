3e236f5c9fce411ac474b8acb2563ae2c138a945
judegiordano <judegiordano@gmail.com>
2022-10-20 10:30:09 -0400

    Static -> getServerSide

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ import { redis } from "@http/redis-agent";
 
 interface HomeProps {
 	count: number
-	last_fetched: string
 }
 
 const formatNumber = (number: number, notation: "standard" | "scientific" | "engineering" | "compact" = "standard") => {
@@ -14,15 +13,13 @@ const formatNumber = (number: number, notation: "standard" | "scientific" | "eng
 	return formatter.format(number);
 };
 
-const Home = ({ count, last_fetched }: HomeProps) => {
+const Home = ({ count }: HomeProps) => {
 	return (
 		<div className="h-screen px-5 pt-5 text-center">
 			<div className="m-auto text-center shadow-xl card w-96 hover:cursor-default">
 				<div className="card-body">
 					<div className="stat">
-						<div className="tooltip" data-tip={`last updated: ${last_fetched}`}>
-							<div className="text-6xl font-bold stat-value text-secondary md:text-8xl">{formatNumber(count, "compact")}</div>
-						</div>
+						<div className="text-6xl font-bold stat-value text-secondary md:text-8xl">{formatNumber(count, "compact")}</div>
 						<div className="stat-desc font-thin text-[20px] pt-3"><span className="font-medium">{formatNumber(count)}</span> lines of code</div>
 					</div>
 				</div>
@@ -35,15 +32,10 @@ const Home = ({ count, last_fetched }: HomeProps) => {
 	);
 };
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
 	const { result: count } = await redis.get<{ result: string }>("GET/lines_of_code");
 	return {
-		props: {
-			count,
-			last_fetched: new Date().toLocaleString()
-		},
-		// 1 hour in seconds
-		revalidate: 3_600
+		props: { count }
 	};
 }
 
