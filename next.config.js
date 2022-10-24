d91d51a5bf7f90322f39e5540a28547caf975cf8
judegiordano <judegiordano@gmail.com>
2022-10-24 11:54:56 -0400

    Security Headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,49 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';
+`;
+
 /** @type {import('next').NextConfig} */
 module.exports = {
-	reactStrictMode: true,
 	swcMinify: true,
-	trailingSlash: false
+	reactStrictMode: true,
+	trailingSlash: false,
+	poweredByHeader: false,
+	headers: [
+		{
+			key: "X-XSS-Protection",
+			value: "1; mode=block"
+		},
+		{
+			key: "X-DNS-Prefetch-Control",
+			value: "on"
+		},
+		{
+			key: "Strict-Transport-Security",
+			value: "max-age=63072000; includeSubDomains; preload"
+		},
+		{
+			key: "X-Frame-Options",
+			value: "SAMEORIGIN"
+		},
+		{
+			key: "Permissions-Policy",
+			value: "camera=(), microphone=(), geolocation=(), browsing-topics=()"
+		},
+		{
+			key: "X-Content-Type-Options",
+			value: "nosniff"
+		},
+		{
+			key: "Referrer-Policy",
+			value: "origin-when-cross-origin"
+		},
+		{
+			key: "Content-Security-Policy",
+			value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim()
+		}
+	]
 };
